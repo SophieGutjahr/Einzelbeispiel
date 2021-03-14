3cf127ab0c9742f0c1d727e9882f6fc41a6dd60e
SophieGutjahr <angutjahr@edu.aau.at>
2021-03-14 09:32:50 +0100

    Berechnung

--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -21,14 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendenKlick(View view){
+    public void sendenKlick(View view) throws InterruptedException {
         textView = findViewById(R.id.editTextNumber);
 
-        Runnable runnable = new Runnable(textView.getText().toString());
-        new Thread(runnable).start();
-        returnValue = runnable.Eingabe;
+        RunnableTCP runnable = new RunnableTCP(textView.getText().toString());
 
+        new Thread(runnable).start();
+        Thread.sleep(500);
         AlertDialog.Builder popFenster = new AlertDialog.Builder(this);
+        returnValue = runnable.NutzerVariable;
+
+
         popFenster.setTitle("Antwort vom Server");
         popFenster.setMessage(returnValue);
         popFenster.setCancelable(true);
@@ -36,7 +39,56 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog showFenster = popFenster.show();
         showFenster.show();
     }
-    public void BerechnungKlick(View view){
 
+
+    public void Berechnung(View view){
+
+    textView = findViewById(R.id.editTextNumber);
+
+    String BerechnungsWert = textView.getText().toString();
+    char [] BerechnungsArray = BerechnungsWert.toCharArray();
+
+    String rueckgabewert = "";
+
+        char [] ErgebnisArray = new char [7];
+
+
+        if (BerechnungsArray[0]=='2'|| BerechnungsArray[0]=='3' || BerechnungsArray[0]=='5'|| BerechnungsArray[0]=='7' ){
+            ErgebnisArray [0] = BerechnungsArray [0];
+        }
+        if (BerechnungsArray [1] =='2'||BerechnungsArray [1] == '3'||BerechnungsArray [1] == '5'||BerechnungsArray [1] == '7'){
+            ErgebnisArray[1]= BerechnungsArray [1];
+        }
+        if (BerechnungsArray[2]=='2'|| BerechnungsArray[2]=='3' || BerechnungsArray[2]=='5'|| BerechnungsArray[2]=='7' ){
+            ErgebnisArray[2]=BerechnungsArray[2];
+        }
+        if (BerechnungsArray[3]=='2'|| BerechnungsArray[3]=='3' || BerechnungsArray[3]=='5'|| BerechnungsArray[3]=='7' ){
+            ErgebnisArray[3]=BerechnungsArray[3];
+        }
+        if (BerechnungsArray[4]=='2'|| BerechnungsArray[4]=='3' || BerechnungsArray[4]=='5'|| BerechnungsArray[4]=='7' ){
+            ErgebnisArray[4]=BerechnungsArray[4];
+        }
+        if (BerechnungsArray[5]=='2'|| BerechnungsArray[5]=='3' || BerechnungsArray[5]=='5'|| BerechnungsArray[5]=='7' ){
+            ErgebnisArray[5]=BerechnungsArray[5];
+        }
+        if (BerechnungsArray[6]=='2'|| BerechnungsArray[6]=='3' || BerechnungsArray[6]=='5'|| BerechnungsArray[6]=='7' ){
+            ErgebnisArray[6]=BerechnungsArray[6];
+        }
+        if (BerechnungsArray[7]=='2'|| BerechnungsArray[7]=='3' || BerechnungsArray[7]=='5'|| BerechnungsArray[7]=='7' ){
+            ErgebnisArray[7]=BerechnungsArray[7];
+        }
+        for(int i = 0; i<ErgebnisArray.length; i++){
+            if (ErgebnisArray[i]!= '0'){
+
+            }
+        }
+        rueckgabewert.concat(String.valueOf(ErgebnisArray));
+
+     AlertDialog.Builder popFenster = new AlertDialog.Builder(this);
+    popFenster.setTitle("Antwort vom Server");
+    popFenster.setMessage(rueckgabewert);
+       popFenster.setCancelable(true);
+       AlertDialog showFenster = popFenster.show();
+       showFenster.show();
     }
 }
